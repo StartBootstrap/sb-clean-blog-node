8244dbf7b43fc1e8613315eae875ceaa006c763a
Amol Nanaware <nanawareamol33@gmail.com>
2020-08-20 19:25:03 +0100

    query formation error
    query included $NEW_DB as a string not the variable value.

--- scripts/db/db-reset.ts
+++ scripts/db/db-reset.ts
@@ -5,8 +5,8 @@ import { _handleShellResult } from '../_shared';
 process.env.NEW_DB = process.env.TYPE_ORM_DATABASE;
 process.env.TYPE_ORM_DATABASE = 'template1';
 
-_handleShellResult(sh.exec(`npm run cli -- query "DROP DATABASE IF EXISTS $NEW_DB"`));
-_handleShellResult(sh.exec(`npm run cli -- query "CREATE DATABASE $NEW_DB"`));
+_handleShellResult(sh.exec(`npm run cli -- query "DROP DATABASE IF EXISTS "` + process.env.NEW_DB));
+_handleShellResult(sh.exec(`npm run cli -- query "CREATE DATABASE "` + process.env.NEW_DB));
 
 process.env.TYPE_ORM_DATABASE = process.env.NEW_DB;
 
